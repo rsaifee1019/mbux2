8a97b55d5a262128a91a0112b173f599e4adefba
Saifee Rahman Saif <saifeer1019@gmail.com>
2024-11-17 17:14:11 +0600

    teachers

--- src/app/api/teachersAll/route.js
+++ src/app/api/teachersAll/route.js
@@ -5,6 +5,12 @@ import connectionToDatabase from '@/lib/mongodb';
 export async function GET(request) {
   await connectionToDatabase();
   const teachers = await Teacher3.find();
-  console.log('Teachers data:', teachers);
-  return NextResponse.json(teachers);
+
+  // Set headers to prevent caching
+  const response = NextResponse.json(teachers);
+  response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  response.headers.set('Pragma', 'no-cache');
+  response.headers.set('Expires', '0');
+
+  return response;
 }
