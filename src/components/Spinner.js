853a2cf9dd2edb826e3b6fc2b81f777a4ee85cc7
Saifee Rahman Saif <saifeer1019@gmail.com>
2025-03-13 13:31:22 +0600

    styles

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,6 +4,6 @@ import { Loader2 } from "lucide-react"
 
 export default function Spinner() {
     return <div className="flex justify-center items-center h-[50vh] w-[95vw]">
-    <Image src="/spinner.svg" alt="Loading..." width={100} height={100} />
+    <Image src="/Spinner.svg" alt="Loading..." width={100} height={100} />
     </div>
 }
